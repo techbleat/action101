dc482881ed7f135b07fd9e96a2b075cda8054d6c
Olusegun <shegoj@yahoo.com>
2024-10-27 21:16:23 +0000

    updated code to create EC2

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
-provider  "aws" {
-    region = "eu-west-2"
+provider "aws" {
+  region = "eu-west-2"
+}
+resource "aws_instance" "ec-create" {
+  ami           = "ami-0b4c7755cdf0d9219"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Node-Created-By-GitAction"
+  }
+
 }
